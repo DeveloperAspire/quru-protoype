ef8de80cc75fcaf50080b8eec624ef6afed29249
Franklin <franklinokolie86@gmail.com>
2021-02-01 09:52:53 +0100

    Made a correction to the font-name

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
         'darkyellow':'#F8B336',
       },
       fontfamily:{
-        'font-family': ['Work Sans', 'sans-serif']
+        'work': ['Work Sans', 'sans-serif']
       }
     },
   },

9050861b3d38458fe4aefe72e3e591ee16c51f54
Franklin <franklinokolie86@gmail.com>
2021-02-01 09:45:44 +0100

    Configured colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'blue':'#2B3273',
+        'yellow': '#FACC70',
+        'darkyellow':'#F8B336',
+      },
+      fontfamily:{
+        'font-family': ['Work Sans', 'sans-serif']
+      }
+    },
   },
   variants: {
     extend: {},
